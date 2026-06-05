0d63b707778f4dd0966ab7a41829dbf497e5e94c
Kateryna <katerina25071984@gmail.com>
2026-06-05 23:48:48 +0300

    01-ts-basics

---
+++ 01-ts-basics/src/task-6.ts
@@ -0,0 +1,11 @@
+function getFirstElement<T>(arr: T[]): T {
+  return arr[0];
+}
+
+getFirstElement<number>([1, 2, 3]);
+getFirstElement<string>(["a", "b", "c"]);
+getFirstElement<boolean>([true, false, true]);
+
+console.log(getFirstElement<number>([1, 2, 3]));
+console.log(getFirstElement<string>(["a", "b", "c"]));
+console.log(getFirstElement<boolean>([true, false, true]));
